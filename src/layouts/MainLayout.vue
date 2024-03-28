e32e1b47a2b866787489e6f81b4b08d18549e99c
xing-6464 <17621404964@163.com>
2024-03-28 22:17:23 +0800

    chore(路由开发): 创建3个Layout和使用Outlet

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>MainLayout header</div>
+  <div>
+    <router-view />
+  </div>
+  <div>MainLayout footer</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss"></style>
---
+++ src/layouts/ManageLayout.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <div class="left">
+      <p>ManageLayout left</p>
+      <button>创建问卷</button>
+      <br />
+      <a>我的问卷</a><br />
+      <a>星标问卷</a><br />
+      <a>回收站</a>
+    </div>
+    <div class="right">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.container {
+  padding: 24px 0;
+  width: 1200px;
+  margin: 0 auto;
+  display: flex;
+}
+
+.left {
+  width: 120px;
+}
+
+.right {
+  flex: 1;
+  margin-left: 60px;
+}
+</style>
