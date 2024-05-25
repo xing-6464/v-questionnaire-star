c42253425e68d2a7beff6040f452907fe29f8cf0
xing-6464 <17621404964@163.com>
2024-05-26 05:39:37 +0800

    chore: 开发获取用户信息，以及退出登录

--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -1,7 +1,32 @@
 <template>
-  <router-link to="/login">登录</router-link>
+  <template v-if="data && data.username">
+    <span style="color: #e8e8e8">
+      <UserOutlined />
+      {{ data.nickname }}
+    </span>
+    <AButton type="link" @click="logout">退出</AButton>
+  </template>
+  <template v-else>
+    <router-link :to="LOGIN_PATHNAME">登录</router-link>
+  </template>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { LOGIN_PATHNAME } from '@/router'
+import { getUserInfoService } from '@/services/user'
+import { removeToken } from '@/utils/user-token'
+import { UserOutlined } from '@ant-design/icons-vue'
+import { message } from 'ant-design-vue'
+import { useRequest } from 'vue-request'
+import { useRouter } from 'vue-router'
 
-<style scoped lang="scss"></style>
+const router = useRouter()
+const { data } = useRequest(getUserInfoService)
+
+function logout() {
+  // 清除token
+  removeToken()
+  router.push(LOGIN_PATHNAME)
+  message.success('退出成功')
+}
+</script>
