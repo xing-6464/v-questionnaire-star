596f17c20101109080a1123a1b6d0ef5b3c74448
xing-6464 <17621404964@163.com>
2024-05-25 19:57:11 +0800

    chore: 开发分页功能-定义URL参数page和pageSize

--- src/hooks/useLoadQuestionListData.ts
+++ src/hooks/useLoadQuestionListData.ts
@@ -1,7 +1,7 @@
 import { useRequest } from 'vue-request'
 import { useRoute } from 'vue-router'
 import { getQuestionListService } from '@/services/question'
-import { LIST_SEARCH_PARAM_KEY } from '@/constant'
+import { LIST_PAGE_SIZE_PARAM_KEY, LIST_SEARCH_PARAM_KEY, PAGE_SIZE } from '@/constant'
 type OptionType = {
   isStar: boolean
   isDeleted: boolean
@@ -14,8 +14,17 @@ function useLoadQuestionListData(opt: Partial<OptionType> = {}) {
   const { data, error, loading } = useRequest(
     async () => {
       const keyword = route.query[LIST_SEARCH_PARAM_KEY] || ''
+      const page = parseInt((route.query[LIST_SEARCH_PARAM_KEY] || '') as string) || 1
+      const pageSize =
+        parseInt((route.query[LIST_PAGE_SIZE_PARAM_KEY] || '') as string) || PAGE_SIZE
 
-      const data = await getQuestionListService({ keyword, isStar, isDeleted } as OptionType)
+      const data = await getQuestionListService({
+        keyword,
+        isStar,
+        isDeleted,
+        page,
+        pageSize
+      } as OptionType)
       return data
     },
     {
