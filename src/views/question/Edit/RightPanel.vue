a85579cb4e44cc3ff6153252fcd530bb3b84e330
xing-6464 <17621404964@163.com>
2024-05-29 14:33:22 +0800

    chore: 动态切换右侧tabs-根据选中组件动态切换属性和页面设置

--- src/views/question/Edit/RightPanel.vue
+++ src/views/question/Edit/RightPanel.vue
@@ -1,6 +1,6 @@
 <template>
-  <a-tabs default-active-key="prop">
-    <a-tab-pane key="prop">
+  <a-tabs :active-key="activeKey">
+    <a-tab-pane :key="TAB_KEYS.PROP_KEY">
       <template #tab>
         <span>
           <FileTextOutlined />
@@ -9,19 +9,35 @@
       </template>
       <ComponentProp />
     </a-tab-pane>
-    <a-tab-pane key="setting">
+    <a-tab-pane :key="TAB_KEYS.SETTING_KEY">
       <template #tab>
         <span>
           <SettingOutlined />
           页面设置
         </span>
       </template>
-      <div>设置</div>
+      <PageSetting />
     </a-tab-pane>
   </a-tabs>
 </template>
 
 <script setup lang="ts">
+import { ref, watch } from 'vue'
 import { FileTextOutlined, SettingOutlined } from '@ant-design/icons-vue'
 import ComponentProp from './ComponentProp.vue'
+import PageSetting from './PageSetting.vue'
+import useGetComponentInfo from '@/hooks/useGetComponentInfo'
+
+enum TAB_KEYS {
+  PROP_KEY = 'prop',
+  SETTING_KEY = 'setting'
+}
+
+const activeKey = ref(TAB_KEYS.PROP_KEY)
+const { selectedId } = useGetComponentInfo()
+
+watch(selectedId, (newVal) => {
+  if (newVal) activeKey.value = TAB_KEYS.PROP_KEY
+  else activeKey.value = TAB_KEYS.SETTING_KEY
+})
 </script>
